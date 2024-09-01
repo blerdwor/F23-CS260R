ec908c1976f450a837ac878a2e36ab7fb0d56401
Belle L. <53383802+blerdwor@users.noreply.github.com>
2024-08-31 20:23:32 -0700

    Delete train_ppo_in_singleagent_env.sh

--- train_ppo_in_singleagent_env.sh
+++
@@ -1,7 +0,0 @@
-# If I want to train an agent from scratch:
-
-python train_ppo_in_singleagent_env.py \
---log-dir train_ppo_in_single_agent_env \
---num-processes 10 \
---num-steps 4_000 \
---max-steps 10_000_000
